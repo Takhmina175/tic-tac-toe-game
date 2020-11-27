8dca51c2c7a51e27e9398c08658e510eb5478c32
mina <takhminam@gmail.com>
2020-11-28 02:33:46 +0300

    create basic flow of the game

--- bin/main.rb
+++ bin/main.rb
@@ -1,52 +1,65 @@
 #!/usr/bin/env ruby
 puts "Hello World!"
-class TicTacToe
-   attr_accessor :board
-   def initialize
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-   end
+class Player
+    attr_accessor :board
+  
+    def initialize
+      @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    end
+
     def display_board
-        puts "#{@board[0]} |#{@board[1]} |#{@board[2]}"
-        puts "---------------"
-        puts "#{@board[3]} |#{@board[4]} |#{@board[5]}"
-        puts "---------------"
-        puts "#{@board[6]} |#{@board[7]} |#{@board[8]}"
-    end
-    def user_input
-        puts "Choose a spot"
-        input = gets.chomp.to_i - 1
-    end
-    def current_player(user_input)
-        if @board[user_input] == 'X' 
-        @board[user_input] = "X"
-        else
-            @board[user_input] = "O"
-        end
-    end
+      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+      puts " ----------- "
+      puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+      puts " ----------- "
+      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end   
 
-    def board_position?(input)
-        if @board[input] == 'X' || @board[input] == 'O' 
-        end
-    end
-
-    def valid_move?(input)
-        input.between?(0, 8) && !board_position?(input)
-    end
-
-    def board_full?
-        index = @board.length-1
-            if index > 8
-           puts "Game is over"
-            end
-        end
-    end
-    def winner?(@board, current_player)
-            if @board[0] == player && @board[1] == player && @board[2] == player||
-               @board[3] == player && @board[4] == player && @board[5] == player||
-               @board[6] == player && @board[7] == player && @board[8] == player
-                puts "You won the Game!"
-    end
-
+def turn?(num)
+  if num < 0 || num > 8 
+    user_input
+  elsif position_taken?(num)
+    puts "position is already taken choose another number"
+    user_input
+  elsif turn_count() % 2 == 0
+    @board[num] = "X"
+  else
+    @board[num] = "O"
+  end
 end
+
+def winner?(curr_player)
+  if @board[0] == curr_player && @board[1] == curr_player && @board[2] == curr_player
+  end
+end
+
+def turn_count
+  taken = 0
+  @board.each do |i|
+    if i == "X" || i == "O"
+      taken += 1
+    end
+  end
+  return taken
+end
+
+def position_taken?(input)
+  @board[input] == "X" || @board[input] == "O"
+end
+
+def user_input
+  puts "Choose a spot"
+  input = gets.chomp.to_i-1 
+  if turn?(input)
+    display_board
+  elsif winner?(input)
+    puts "You won the game"
+  end
+  user_input
+end
+  
+end
+player = Player.new
+player.user_input
 
 
